e70d7889d809c644efaa0e2995534a5da45c36bc
NoobPatata <noobman2613@gmail.com>
2020-03-23 05:44:57 +0800

    Created function to insert into prescription , prescription ID , Invoice and Invoice Details

---
+++ Oceana (WPF)/ViewModels/Doctor/DetailsID.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Collections.ObjectModel
+
+Public Class DetailsID
+
+    Inherits PropertyChanged
+    Private _DID As Integer
+    Private v As Object
+
+
+
+    Public Sub New(dID As Integer)
+        _DID = dID
+    End Sub
+
+    Public Property DID As Integer
+        Get
+            Return _DID
+        End Get
+        Set(value As Integer)
+            _DID = value
+            OnPropertyChanged(DID)
+        End Set
+    End Property
+End Class
+
+Public Class ObservableDetailsID
+    Inherits ObservableCollection(Of DetailsID)
+End Class
---
+++ Oceana (WPF)/ViewModels/Doctor/PrescriptionTreatments.vb
@@ -0,0 +1,3 @@
+﻿Public Class PrescriptionTreatments
+
+End Class
